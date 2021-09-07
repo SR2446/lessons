278c7ba7fbe541bc2bdae165c826e35263dffac9
Saiful Rony <75594417+SR2446@users.noreply.github.com>
2021-09-07 12:26:29 +0900

    Add files via upload
    new

---
+++ (PHP)ServerSiteProgramming/lesson01/item.php
@@ -0,0 +1,8 @@
+<?php
+    echo 'コーヒー';
+    echo PHP_EOL;
+    echo '紅茶';
+    echo"\n";
+    echo 'ほうじ茶';
+
+?>
---
+++ (PHP)ServerSiteProgramming/lesson01/simple.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>this is simple php</h2>
+    
+    <?php
+      echo 'こんにちは';
+      echo 1+2;
+      ?>
+    
+    <p>
+        <?php
+            echo '神奈川県'.'横浜市';
+        ?>
+    </p>
+
+</body>
+</html>
